a3c8d0480626ef1b74ce89b82db3ec65052090aa
Pravin Tangade <99708895+Pravin0206@users.noreply.github.com>
2024-11-12 09:28:29 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treats all js code as per newer version.
+
+// alert(6+6) // As we are nodejs and not the browser it gives error
+
+// Code reabaability should be high....
+
+// DATATYPES:-
+
+// number => 2^53
+let num = 33
+
+// bigint => for larger numeric values 
+
+// string => "  "
+let name = "xyz"
+
+// boolean => true / false
+let flag = true
+
+// null => standalone value
+let goal = null
+
+// undefined => value is not assigned
+let count;
+
+// symbol => used to store unique values
+
+// object
+
+console.log(typeof undefined) // Undefined datatype
+
+// console.log(typeof(goal))
+console.log(typeof null) // Object datatype
