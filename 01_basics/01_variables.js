9486c01dcde11fcda737b380ec00561f7f6313d1
Pravin Tangade <99708895+Pravin0206@users.noreply.github.com>
2024-11-12 09:29:02 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const id = 123
+let email = "xyz@com"
+var password = "abccacx"
+city = "Pune"
+let state
+
+// Don't use var keyword as creates problems for block scope and functional scope..
+
+console.log(id)
+
+email = "yz@jncj.com"
+password = "abcabc"
+
+console.table([id,email,password,city,state])
