d6a2eb42d9c16219a35fb06305a692586e6a7ab4
Pravin Tangade <99708895+Pravin0206@users.noreply.github.com>
2024-11-14 06:31:12 +0000

    Datatype Summary

---
+++ 01_basics/05_datatypeSummary.js
@@ -0,0 +1,43 @@
+// Datatypes are of two type :-
+
+// 1.Primitive 
+
+// String
+const name = "xyz"
+
+// Number
+const score = 20
+const percentage = 90.24
+
+// Boolean 
+const getId = true
+
+// Null
+let temp = null
+
+// Undefined
+let total;
+
+// Symbol
+const id = Symbol("123")
+const ids = Symbol("123")
+
+// Bigint
+const val = 124555n
+
+// 2.Non-primitive/ Reference Datatype
+
+// Arrays
+const hereos = ["Batman","superman","thor"]
+
+
+//Object
+const myObj = {
+    name:"Pravin",
+    age:23
+}
+
+// Function
+const myFun = function(){
+    console.log("Hello");
+}
