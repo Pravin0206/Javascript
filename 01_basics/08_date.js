1bff2fd36027ef87c3817e6f80577f3f7bc647ca
Pravin Tangade <99708895+Pravin0206@users.noreply.github.com>
2024-11-19 11:52:43 +0000

    date and time

---
+++ 01_basics/08_date.js
@@ -0,0 +1,33 @@
+let myDate = new Date()
+
+//Ways to represent date and time using different methods 
+
+// console.log(myDate);
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+
+// Declare date and time
+
+// let newDate = new Date(2024,5,24,10,54,20)
+// console.log(newDate.toLocaleString());
+
+//Some more ways to declare date and time 
+
+// let newDate = new Date("2024-02-14")
+let newDate = new Date("01-24-2024")
+
+// console.log(newDate.toLocaleString());
+
+// TImestamp
+
+// let timeStamp = Date.now()
+// // console.log(newDate.getTime());
+// console.log(timeStamp);
+
+let date = new Date()
+console.log(date.getFullYear());
+
