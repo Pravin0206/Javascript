f53ffdea10ca1f7861a16c693f5a9200a8f917ed
Pravin Tangade <99708895+Pravin0206@users.noreply.github.com>
2024-11-19 10:49:52 +0000

    numbers and maths

---
+++ 01_basics/07_numbers.js
@@ -0,0 +1,47 @@
+// Basic way
+
+// const num = 34
+
+// const num = new Number(900.878)
+// console.log(num);
+
+// Tostring method
+// console.log(num.toString().length);
+
+// Show how many decimal
+// console.log(num.toFixed(2));
+
+// Give exact precision value as per given number
+// console.log(num.toPrecision(8));
+
+// Convert number to special number system
+// const num1 = 145124555
+// console.log(num1.toLocaleString("en-IN"));
+
+
+
+// --------------+++++++++++++++++++   Maths     ++++++++++++++++-------------------------
+
+// Math is inbuilt library of javascript
+
+// Gives absolute value
+// console.log(Math.abs(-50));
+
+// Gives round off value
+// console.log(Math.round(45.910));
+
+// Gives highest value
+// console.log(Math.ceil(45.30));
+
+// Gives lowest value
+// console.log(Math.floor(45.90));
+
+// Gives mininmum value
+// console.log(Math.min(5,4,8,9,1));
+
+// Gives maximum value
+// console.log(Math.max(5,4,8,9,1));
+
+// Generates random value between 0 and 1
+// console.log(Math.random());
+console.log((Math.random() * 10) + 1);
