975a35f68aef85aeab0269a3eedf14cb4b7dd760
Pravin Tangade <99708895+Pravin0206@users.noreply.github.com>
2024-12-10 08:18:10 +0000

    Nullish Operator

---
+++ 04_control_flow/nullish.js
@@ -0,0 +1,14 @@
+// Nullish Coalescing Opeartor(??) => The nullish coalescing (??) operator is a logical operator that returns its 
+// right-hand side operand when its left-hand side operand is null or undefined, and 
+// otherwise returns its left-hand side operand.
+
+let val1;
+
+// val1 = 10 ?? 20 => 10
+// val1 = null ?? 20 => 20
+// val1 = undefined ?? 20 => 20
+// val1 = undefined ?? 20 ?? 30 => 20
+
+
+
+console.log(val1);
