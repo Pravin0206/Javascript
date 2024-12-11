b651f379fdfe5b64e4e3343808404293238b4f6f
Pravin Tangade <99708895+Pravin0206@users.noreply.github.com>
2024-12-11 13:57:34 +0000

    While Do-while

---
+++ 05_loops/two.js
@@ -0,0 +1,25 @@
+// While loop 
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(index);
+//     index = index + 2
+// }
+
+// let myArr = ["flash","superman","batman","deadpool","wolverine"]
+
+// let arr = 0;
+
+// while (arr < myArr.length) {
+//     console.log(`Value in array: ${myArr[arr]}`);
+//     arr++
+// }
+
+// Do while
+
+let score = 10
+
+do {
+    console.log(`Score:${score}`)
+    score++
+} while (score < 10);
